a0f2835525096f10e509d5a0aa2617683b534be2
Eric Archer <eric.archer@noaa.gov>
2016-12-12 10:33:57 -0800

    editing README

--- v2.params.R
+++
@@ -1,8 +0,0 @@
-# scenario parameters
-label <- "v2"
-num.pops <- 5
-num.loci <- 1000
-div.time <- 50000
-Ne <- c(50, 500) # c(50, 500, 5000)
-Nm <- c(0, 1, 5) # c(0, 1, 5, 25)
-theta <- c(0.1, 0.25)
--- v3.params.R
+++ v3.params.R
@@ -3,6 +3,6 @@ label <- "v3"
 num.pops <- 5
 num.loci <- 1000
 div.time <- 50000
-Ne <- c(50, 500) #, 5000)
-Nm <- c(0, 1, 5) #, 25)
+Ne <- c(50, 500)
+Nm <- c(0, 1, 5)
 theta <- c(0.1, 0.25)
